9fc4509323db78b1e58366b884159b65f1f89ce3
Magda Balun <magi@op.pl>
2018-01-11 13:57:31 +0100

    Change App.js

---
+++ components/App.js
@@ -0,0 +1,85 @@
+var GIPHY_API_URL = 'https://api.giphy.com';
+var GIPHY_PUB_KEY = '40vGucBKoxMCTutX51Ciry0VnJ0IMdie';
+
+
+App = React.createClass({
+    getInitialState: function() {
+        return {
+            loading: false,
+            searchingText: '',
+            gif: {}
+        };
+    },
+
+    handleSearch: function(searchingText) {
+        this.setState({
+            loading: true
+        });
+
+        this.getGif(searchingText, function(gif) {
+            this.setState({
+                loading: false,
+                gif: gif,
+                searchingText: searchingText
+            });
+        }.bind(this));
+    },
+
+    getGif: function(searchingText) {
+        return new Promise((resolve, reject) => {
+            var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;
+            var xhr = new XMLHttpRequest();
+            xhr.open('GET', url);
+            xhr.onload = () => {
+                if (xhr.status === 200) {
+                    var data = JSON.parse(xhr.responseText).data;
+                    var gif = {
+                        url: data.fixed_width_downsampled_url,
+                        sourceUrl: data.url
+                    };
+                    resolve(gif);
+                } else {
+                    reject(new Error(this.error));
+                }
+            };
+            xhr.onerror = () => {
+                reject(new Error(`XMLHttpRequest Error: ${this.error}`));
+            };
+            xhr.send();
+        })
+    },
+
+    render: function() {
+
+        var styles = {
+            margin: '0 auto',
+            textAlign: 'center',
+            width: '90%'
+        };
+
+        return ( <
+            div style = {
+                styles
+            } >
+            <
+            h1 > Gif searcher < /h1> <
+            p > Find gif on < a href = 'http=://giphy' > giphy < /a>. Press ENTER for start search more gifs.</p >
+            <
+            Search onSearch = {
+                this.handleSearch
+            }
+            /> <
+            Gif loading = {
+                this.state.loading
+            }
+            url = {
+                this.state.gif.url
+            }
+            sourceUrl = {
+                this.state.gif.sourceUrl
+            }
+            /> <
+            /div>    
+        )
+    }
+})
